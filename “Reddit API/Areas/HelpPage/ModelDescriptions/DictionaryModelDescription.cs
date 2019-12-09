c2ca36088f3b4b815d11c5d3028a6916214dc88a
Leonards Reinicans <54269623+Skagish@users.noreply.github.com>
2019-12-09 13:07:13 +0200

    "First push

---
+++ “Reddit API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _Reddit_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ “Reddit API/Client/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _Reddit_API.Client
+{
+    public class Client
+    {
+        string client_id = "Skagish";
+        string client_secret = "EYfI97NNVmMRXncS9NyYGPnAsUg";
+    }
+}
